a27037c2179a3fbf86bbe62df09e04df45677f78
niko <nikolas.molinari@gmail.com>
2023-12-11 00:51:41 +0100

    working solution

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import { join } from 'path';
+import { arch, argv } from 'process';
 
 function readLines(filename: string) {
   const result = fs.readFileSync(join("data", filename), 'utf-8');
@@ -7,6 +8,31 @@ function readLines(filename: string) {
   return lines;
 }
 
-const lines = readLines("example");
+const input = argv[argv.length-1];
 
-console.log(lines);
+const lines = readLines(input);
+
+const calib = lines.map(line => {
+  const matcher = /\d/;
+  var first = -1;
+  var last = -1;
+  for (let c of line) {
+    const int = parseInt(c);
+    if (!Number.isNaN(int)) {
+      if (first < 0) {
+        first = int;
+        last = int;
+      } else {
+        last = int;
+      }
+    }
+  }
+  if (first < 0 || last < 0) return 0;
+  return first*10+last;
+})
+
+const result = calib.reduce((acc, curr) => {
+  return acc + curr;
+}, 0);
+
+console.log("RESULT:", result);   
