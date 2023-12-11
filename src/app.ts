a552da6c2c88b83041d0b93ad7136c991bb60285
niko <nikolas.molinari@gmail.com>
2023-12-11 01:18:02 +0100

    working solution

--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,24 @@ function readLines(filename: string) {
 
 const input = argv[argv.length-1];
 
+const nuMap = [
+  { "name": "one", "value": 1 },
+  { "name": "two", "value": 2 },
+  { "name": "three", "value": 3 },
+  { "name": "four", "value": 4 },
+  { "name": "five", "value": 5 },
+  { "name": "six", "value": 6 },
+  { "name": "seven", "value": 7 },
+  { "name": "eight", "value": 8 },
+  { "name": "nine", "value": 9 },
+];
+
 const lines = readLines(input);
 
 const calib = lines.map(line => {
-  const matcher = /\d/;
   var first = -1;
   var last = -1;
+  var i = 0
   for (let c of line) {
     const int = parseInt(c);
     if (!Number.isNaN(int)) {
@@ -25,7 +37,20 @@ const calib = lines.map(line => {
       } else {
         last = int;
       }
+    } else {
+      const cutLine = line.substring(i);
+      nuMap.forEach(n => {
+        if (cutLine.indexOf(n.name) == 0) {
+         if (first < 0) {
+           first = n.value;
+           last = n.value;
+         } else {
+           last = n.value;
+         }
+        }
+      })
     }
+    i++;
   }
   if (first < 0 || last < 0) return 0;
   return first*10+last;
